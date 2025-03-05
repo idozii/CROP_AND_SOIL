912983909f3ed884746602571cf346e944465580
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-03-05 10:42:21 +0700

    preprocessor and model

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 #! Load the data
 data = pd.read_csv('data/data.csv')
@@ -12,10 +16,22 @@ print(numerical_cols)
 #! Categorical columns
 categorical_cols = data.select_dtypes(include=[object]).columns.tolist()
 print(categorical_cols)
-#! Plot the data
-plt.figure(figsize=(10, 6))
-plt.scatter(data['Temparature'], data['Crop Type'])
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
 #! Split the data
+X = data.drop(['Crop Type'], axis=1)
+y = data['Crop Type']
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', 'passthrough', numerical_cols),
+        ('cat', OneHotEncoder(), categorical_cols)
+    ])
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('classifier', RandomForestClassifier(random_state=42))
+])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print('Mean Squared Error:', mean_squared_error(y_test, y_pred))
+print('R2 Score:', r2_score(y_test, y_pred))
+print('Mean Absolute Error:', mean_absolute_error(y_test, y_pred))
