62816eeec86116e03f5d2df47cd60010121eeeb3
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-03-05 11:09:27 +0700

    updated

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
 
 #! Load the data
 data = pd.read_csv('data/data.csv')
@@ -68,23 +65,4 @@ def humidity_level(h):
     else:
         return 'Low'
 data['Humidity'] = data['Humidity'].apply(humidity_level)
-#! Split the data
-print(data.head())
-# X = data.drop(['Crop Type'], axis=1)
-# y = data['Crop Type']
 
-# preprocessor = ColumnTransformer(
-#     transformers=[
-#         ('num', 'passthrough', numerical_cols),
-#         ('cat', OneHotEncoder(), categorical_cols)
-#     ])
-# model = Pipeline(steps=[
-#     ('preprocessor', preprocessor),
-#     ('classifier', RandomForestClassifier(random_state=42))
-# ])
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# model.fit(X_train, y_train)
-# y_pred = model.predict(X_test)
-# print('Mean Squared Error:', mean_squared_error(y_test, y_pred))
-# print('R2 Score:', r2_score(y_test, y_pred))
-# print('Mean Absolute Error:', mean_absolute_error(y_test, y_pred))
