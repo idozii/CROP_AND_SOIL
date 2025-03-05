6d5e64852b6f99aa0ba47f8398925c9630d5a1ed
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-03-05 10:35:49 +0700

    plot the data

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 #! Load the data
 data = pd.read_csv('data/data.csv')
-
 #! Numerical columns
 numerical_cols = data.select_dtypes(include=[float, int]).columns.tolist()
 print(numerical_cols)
 #! Categorical columns
 categorical_cols = data.select_dtypes(include=[object]).columns.tolist()
 print(categorical_cols)
+#! Plot the data
+plt.figure(figsize=(10, 6))
+plt.scatter(data['Temparature'], data['Crop Type'])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
 #! Split the data
