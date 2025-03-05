d25c1f5ed7f6de6da82094f6ea757739c5b463dc
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-03-05 10:32:36 +0700

    classify

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
-from keras.callbacks import EarlyStopping
-from keras.layers import Dropout
-from keras.layers import BatchNormalization
-from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 #! Load the data
 data = pd.read_csv('data/data.csv')
-print(data.isnull().sum())
+
+#! Numerical columns
+numerical_cols = data.select_dtypes(include=[np.number]).columns.tolist()
+print(numerical_cols)
+#! Categorical columns
+categorical_cols = data.select_dtypes(include=[np.object]).columns.tolist()
+print(categorical_cols)
+#! Split the data
